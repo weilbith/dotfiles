e5766119713b3be6900857efe2cea2eba67f7b7b
weilbith <thore@weilbier.net>
2018-07-22 16:49:25 +0200

    Provide install script for Shell stuff.
    Installs Zsh itself and establish it as default shell.
    Create soft links for all Zsh configuration files.
    Install Antibody and initially bundle all plugins.
    Install RipGrep.
    Install Fzf and manipulate the scripts to load.

---
+++ zsh/install.sh
@@ -0,0 +1,26 @@
+# Install Zsh.
+sudo apt-get install zsh # For Debian/Ubuntu
+[[ -n "$(cat /etc/shells | grep zsh)" ]] && sudo chsh -s $(which zsh) # Set as default shell.
+
+ZSH_DIR=$HOME/.config/zsh
+mkdir -p $ZSH_DIR
+ln -sf $(pwd)/* $ZSH_DIR/
+ln -sf $(pwd)/.zshrc $HOME/
+
+# Install Antibody and all Zsh plugins.
+curl -sL git.io/antibody | sh -s
+antibody bundle < $(pwd)/plugin-list.txt > $(pwd)/plugin-list.sh
+
+# Install RipGrep.
+RG_DEB=/tmp/ripgrep.deb
+curl -L https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb > $RG_DEB
+sudo dpkg -i ripgrep_0.8.1_amd64.deb
+
+# Install Fzf
+FZF_DIR=$HOME/Tools/fzf
+mkdir -p $FZF_DIR
+git clone --depth 1 https://github.com/junegunn/fzf.git $FZF_DIR
+bash $FZF_DIR/install
+mv ~/.fzf* $FZF_DIR/
+
+
