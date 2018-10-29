f6d76c109c6978c5b4d79ed1eb367bb02497e2ba
Thore Weilbier <thore@weilbier.net>
2018-10-29 14:37:20 +0100

    Restructure organizer tools.
    Rename directory from 'mail' to more general 'organizer'.
    Create own folder for each tool which use its own directory at
    $XDG_CONFIG_HOME to make it easier.
    Move NeoMutts configuration to the $XDG_CONFIG_HOME as well, which
    required an adjustment of multiple configuration files using the account
    files.
    Restructure the installation script and add some comments.

---
+++ organizer/install.sh
@@ -0,0 +1,54 @@
+# Install packages.
+sudo pacman -S --noconfirm neomutt isync msmtp notmuch lynx khard khal
+trizen -S vdirsyncer ripmime urlscan-git
+pip install --user requests-oauthlib
+
+# Define directory variables.
+XDG_CONFIG_HOME=$HOME/.config
+XDG_CACHE_HOME=$HOME/.cache
+
+MAIL_DIR=$HOME/Mail
+CONTACT_DIR=$HOME/Contacts
+CALENDAR_DIR=$HOME/Calendar
+
+MUTT_CACHE=$XDG_CACHE_HOME/mutt
+VDIRSYNCER_CACHE=$XDG_CACHE_HOME/vdirsyncer
+ACC_CONFIG_DIR=$MUTT_DIR/accounts
+NOTMUCH_DATABASE_PATH=$MAIL_DIR/.notmuch
+
+# Create directories.
+mkdir -p $MAIL_DIR
+mkdir -p $CONTACT_DIR/brainbot_gsuite
+mkdir -p $CONTACT_DIR/private_nextcloud
+
+mkdir -p $MUTT_CACHE
+mkdir -p $ACC_CONFIG_DIR
+mkdir -p $NOTMUCH_DATABASE_PATH
+mkdir -p $VDIRSYNCER_CACHE
+
+# Link dotfiles to the respective locations.
+ln -sf $(pwd)/mailcap $HOME/.mailcap
+ln -sf $(pwd)/mbsyncrc $HOME/.mbsyncrc
+ln -sf $(pwd)/msmtprc $HOME/.msmtprc
+ln -sf $(pwd)/notmuch/notmuch-config $HOME/.notmuch-config
+ln -sf $(pwd)/notmuch/hooks $NOTMUCH_DATABASE_PATH/
+
+ln -sf $(pwd)/mutt $XDG_CONFIG_HOME/
+ln -sf $(pwd)/vdirsyncer $XDG_CONFIG_HOME/
+ln -sf $(pwd)/khal $XDG_CONFIG_HOME/
+ln -sf $(pwd)/khard $XDG_CONFIG_HOME/
+ln -sf $(pwd)/urlscan $XDG_CONFIG_HOME/
+
+
+# Make sure the Notmuch hooks are executable by user.
+chmod 700 $(pwd)/notmuch/hooks/*
+
+
+# Getting further:
+# Checkout the PowerStatus10k segments for Khal and Notmuch.
+# Checkout fcron for synchronize everything periodically.
+
+
+# Get a servicers certificate(-fingerprint):
+#openssl s_client -connect mail.brainbot.com:25 -starttls smtp < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout | cut -d'=' -f2
+#openssl s_client -connect mail.brainbot.com:20025 -starttls smtp | sed -ne '/--BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
