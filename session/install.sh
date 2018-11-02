d5bd372d10ddb475025771c9f26f46424024ef18
Thore Weilbier <thore@weilbier.net>
2018-11-02 15:56:36 +0100

    XRG specification adjustments for session directory.

--- session/install.sh
+++ session/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install packages.
-sudo pacman -S xorg xsel xclip xautolock i3-gaps compoton dmenu pulseaudio dex
+sudo pacman -S xorg xsel xclip xautolock compoton dmenu pulseaudio dex
 trizen -S i3lock-color nerd-fonts-source-code-pro rxvt-unicode-patched 
 
 # Xresources
@@ -13,40 +13,26 @@ ln -sf $(pwd)/Xresources $HOME/.Xresources
 ln -sf $(pwd)/Xmodmap $HOME/.Xmodmap
 ln -sf $(pwd)/Xresources.d $HOME/.Xresources.d
 
-# i3 configuration
-I3_CONFIG_DIR=$HOME/.config/i3
-mkdir -p $I3_CONFIG_DIR
-ln -sf $(pwd)/i3config $I3_CONFIG_DIR/config
-
-
-# Install Powerstatus10k
-POWERSTATUS10K_DIR=$HOME/Tools/powerstatus10k
-
-if [[ ! -d "$POWERSTATUS10K_DIR" ]] ; then
-  mkdir -p $POWERSTATUS10K_DIR
-  git clone --depth 1 --recurse-submodules https://github.com/weilbith/powerstatus10k.git $POWERSTATUS10K_DIR
-  base=$(pwd)
-  cd $POWERSTATUS10K_DIR/bar
-  sudo make
-  sudo make install
-  cd $base
-fi
-
-ln -sf $(pwd)/powerstatus10k_custom.conf $POWERSTATUS10K_DIR/config/custom.conf
-chmod +x $POWERSTATUS10K_DIR/powerstatus10k.sh
+# i3
+sudo pacman -S i3-gaps
+ln -sf $(pwd)/i3 $XDG_CONFIG_HOME/
 
 # Dunst
 sudo pacman -S --noconfirm dunst
-DUNST_CONFIG_DIR=$HOME/.config/dunst/
-mkdir -p $DUNST_CONFIG_DIR
-ln -sf $(pwd)/dunstrc $DUNST_CONDIG_DIR
+ln -sf $(pwd)/dunst $XDG_CONFIG_HOME/
 
 # RedShift
 trizen -S redshift-minimal
-RS_DIR=$HOME/.config/redshift
-mkdir -p $RS_DIR
-ln -sf $(pwd)/redshift.conf $RS_DIR/
+ln -sf $(pwd)/redshift $XDG_CONFIG_HOME/
 
+# Link desktop files for CLI based applications.
+sudo ln -sf $(pwd)/desktop-files/* /usr/share/applications/
+
+# Autostart
+ln -sf $(pwd)/autostart $XDG_CONFIG_HOME/
+
+
+# Messy stuff
 # Cerebro
 # Install dependencies.
 sudo pacman -S nodejs yarn fuse readline
@@ -62,8 +48,17 @@ cd $CEREBRO_DIR/plugins
 yarn install
 
 
-# Link desktop files for CLI based applications.
-sudo ln -sf $(pwd)/desktop-files/* /usr/share/applications/
+# Install Powerstatus10k
+POWERSTATUS10K_DIR=$HOME/Tools/powerstatus10k
 
-# Autostart
-ln -sf $(pwd)/autostart $HOME/.config/
+if [[ ! -d "$POWERSTATUS10K_DIR" ]] ; then
+  mkdir -p $POWERSTATUS10K_DIR
+  git clone --depth 1 --recurse-submodules https://github.com/weilbith/powerstatus10k.git $POWERSTATUS10K_DIR
+  base=$(pwd)
+  cd $POWERSTATUS10K_DIR/bar
+  sudo make
+  sudo make install
+  cd $base
+fi
+
+ln -sf $(pwd)/powerstatus10k/powerstatus10k.conf $POWERSTATUS10K_DIR/config/custom.conf
