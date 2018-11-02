1af01d46a3f3bc6412a01eb411d59010f46a9151
Thore Weilbier <thore@weilbier.net>
2018-11-02 13:56:47 +0100

    Establish data directory.
    Merge directories mc and mounting to one.
    Separate configurations by XDG specification.
    Merge and adjust installation instructions.

---
+++ data/install.sh
@@ -0,0 +1,17 @@
+# UDisk
+sudo pacman -S --noconfirm udisks2
+sudo ln -sf $(pwd)/udisk/99-udisks2.rules /etc/udev/rules.d/
+sudo ln -sf $(pwd)/usikd/media.conf /etc/tmpfiles.d/
+sudo systemctl enable udisks2.service
+sudo systemctl start udisks2.service
+
+# BashMount
+trizen -S bashmount
+ln -sf $(pwd)/bashmount $XDG_CONFIG_HOME/
+
+# Midnight Commander
+sudo pacman -S --noconfirm mc
+ln -sf $(pwd)/mc $XDG_CONFIG_HOME/
+$MC_SHARE=$HOME/.local/share/mc
+mkdir -p $MC_SHARE
+ln -sf $(pwd)/mc/skins $MC_SHARE/
--- mc/install.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -S --noconfirm mc
-$CONFIG_DIR=$HOME/.local/share/mc/
-mkdir -p $CONFIG_DIR/skins
-ln -sf $(pwd)/blue_planet.ini $CONFIG_DIR/skins/
-ln -sf $(pwd)/ini $HOME/.config/mc/
-ln -sf $(pwd)/mc.keymap $HOME/.config/mc/
-
