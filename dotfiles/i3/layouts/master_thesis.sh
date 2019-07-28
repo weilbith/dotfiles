325d82069e64f38bedd6b569edd482e920c5559d
Thore Weilbier <thore@weilbier.net>
2019-07-28 13:04:22 +0200

    Integrate session profile

---
+++ dotfiles/i3/layouts/master_thesis.sh
@@ -0,0 +1,3 @@
+kitty --detach nvim
+zathura ~/Uni/11-Semester/Masterarbeit/expose/Expose/Sources/Expose.pdf
+zathura ~/Uni/11-Semester/Masterarbeit/expose/papers/DuCrest_ScalabilityAndTrustlinesArchitecture.pdf
