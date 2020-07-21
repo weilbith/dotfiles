1d177a78e395b04766afd6b80f20de6a02cf9c7c
Thore Weilbier <thore@weilbier.net>
2020-07-21 11:11:57 +0200

    Start with roles for desktop playbook

--- old_to_migrate/dotfiles/i3/layouts/master_thesis.sh
+++
@@ -1,3 +0,0 @@
-kitty --detach nvim
-zathura ~/Uni/11-Semester/Masterarbeit/expose/Expose/Sources/Expose.pdf
-zathura ~/Uni/11-Semester/Masterarbeit/expose/papers/DuCrest_ScalabilityAndTrustlinesArchitecture.pdf
