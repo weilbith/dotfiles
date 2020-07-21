400bca707a575ca3c0324dfd9f5c72bf04809ca7
Thore Weilbier <thore@weilbier.net>
2020-07-21 20:26:25 +0200

    Rework lookup plugin for more general purpose

--- plugins/lookup/role_directory_entries.py
+++ plugins/lookup/role_directory_entries.py
@@ -6,14 +6,6 @@ from typing import List
 from ansible.plugins.lookup import LookupBase
 
 
-def get_files_in_directory(path: str) -> List[str]:
-    if os.path.exists(path):
-        return [entry.path for entry in os.scandir(path) if entry.is_file()]
-
-    else:
-        return []
-
-
 class LookupModule(LookupBase):
     def run(self, terms, variables=None, **kwargs) -> List[str]:
         result = []
@@ -21,6 +13,8 @@ class LookupModule(LookupBase):
         for directory_name in terms:
             for search_path in variables.get("ansible_search_path", []):
                 directory_path = os.path.join(search_path, "files", directory_name)
-                result.extend(get_files_in_directory(directory_path))
+
+                if os.path.exists(directory_path):
+                    result.extend([entry.path for entry in os.scandir(directory_path)])
 
         return result
