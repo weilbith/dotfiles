816336b9df402c89d08182d2f3ff775fa25bd0b9
Thore Weilbier <thore@weilbier.net>
2020-09-26 21:08:44 +0200

    Improve Fzf setup and configuration

--- roles/zplug/files/plugin.d/30-autocomplete.sh
+++ roles/zplug/files/plugin.d/30-autocomplete.sh
@@ -2,4 +2,4 @@ zplug "marlonrichert/zsh-autocomplete"
 
 zstyle ':autocomplete:*' groups 'always'
 zstyle ':autocomplete:(slash|space):*' magic 'off'
-zstyle ':autocomplete:tab:*' completion 'cycle'
+zstyle ':autocomplete:tab:*' completion 'cycle' fzf
