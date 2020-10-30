6c2407db6ddc94412749642d1e05b3860f551be4
Thore Weilbier <thore@weilbier.net>
2020-10-30 11:07:10 +0100

    Add some Zsh plugins for Vi mode to Zinit role

---
+++ roles/zinit/files/plugin.d/30-vi.zsh
@@ -0,0 +1,9 @@
+zinit wait lucid light-mode for \
+  zsh-vi-more/vi-motions \
+  zsh-vi-more/evil-registers \
+  atload"bindkey -M vicmd '  ' vi-easy-motion" \
+  atinit"EASY_MOTION_DIM='fg=239'" \
+  atinit"EASY_MOTION_HIGHLIGHT='fg=191,bold'" \
+  atinit"EASY_MOTION_HIGHLIGHT_2_FIRST='fg=197,bold'" \
+  atinit"EASY_MOTION_HIGHLIGHT_2_SECOND='fg=197,bold'" \
+  IngoMeyer441/zsh-easy-motion
