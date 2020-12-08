c516d826eee654538c692bfb3755a3964a181ba0
Thore Weilbier <thore@weilbier.net>
2020-12-08 20:55:33 +0100

    ssh: use gpg agent to authenticate with YubiKey

--- roles/ssh/files/profile.d/40-ssh-cache.sh
+++
@@ -1,8 +0,0 @@
-# Start agent if not already running and update cache.
-if ! pgrep -u "$USER" ssh-agent >/dev/null; then
-  ssh-agent >"$SSH_AGENT_CACHE_FILE"
-
-# Load agents cache if not already present.
-elif [[ -z "$SSH_AGENT_PID" ]]; then
-  eval "$(<"$SSH_AGENT_CACHE_FILE")" >/dev/null
-fi
