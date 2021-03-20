abe5f234bd6e2d1abc706e5d7683d01f550903f0
Thore Strassburg <thore@weilbier.net>
2021-03-20 14:04:17 +0100

    Zinit: align colors with same functions in Vim

--- roles/zinit/files/plugin.d/30-vi.zsh
+++ roles/zinit/files/plugin.d/30-vi.zsh
@@ -4,11 +4,7 @@ zinit wait lucid light-mode for \
   zsh-vi-more/evil-registers \
   atload"bindkey -M vicmd '  ' vi-easy-motion" \
   atinit"EASY_MOTION_DIM='fg=239'" \
-  atinit"EASY_MOTION_HIGHLIGHT='fg=191,bold'" \
-  atinit"EASY_MOTION_HIGHLIGHT_2_FIRST='fg=197,bold'" \
-  atinit"EASY_MOTION_HIGHLIGHT_2_SECOND='fg=197,bold'" \
+  atinit"EASY_MOTION_HIGHLIGHT=fg=16,bg='#9ed072',bold" \
+  atinit"EASY_MOTION_HIGHLIGHT_2_FIRST=fg=16,bg='#9ed072',bold" \
+  atinit"EASY_MOTION_HIGHLIGHT_2_SECOND=fg=16,bg='#76cce0',bold" \
   IngoMeyer441/zsh-easy-motion
-
-# Does not work?
-#   atinit"zstyle 'zle:exchange' highlight 'fg=239,bg=291'" \
-#   okapia/zsh-viexchange
