200d564c8afbb9150a66b3399069b4340bb103ae
Thore Strassburg <thore@weilbier.net>
2021-03-20 13:22:12 +0100

    Zsh: update autocomplete plugin configuration

--- roles/zinit/files/plugin.d/90-autocomplete.zsh
+++ roles/zinit/files/plugin.d/90-autocomplete.zsh
@@ -1,7 +1,8 @@
 # Can't use `wait` mode, else hooks get not registered for the first prompt.
 zinit lucid light-mode \
   atinit"zstyle ':completion:*' extra-verbose yes" \
-  atinit"zstyle ':autocomplete:*' groups 'always'" \
+  atinit"zstyle ':autocomplete:*' min-input 1" \
   atinit"zstyle ':autocomplete:tab:*' widget-style menu-select" \
   atinit"zstyle ':autocomplete:tab:*' fzf-completion yes" \
+  atinit"zstyle ':autocomplete:tab:*' insert-unambiguous yes" \
   for marlonrichert/zsh-autocomplete
