b8d22cbcf90f621492c4a188f9c9d801e9633976
Thore Strassburg <thore@weilbier.net>
2021-03-28 22:45:40 +0200

    Zinit: change autocomplete plugin configuration

--- roles/zinit/files/plugin.d/90-autocomplete.zsh
+++ roles/zinit/files/plugin.d/90-autocomplete.zsh
@@ -4,5 +4,4 @@ zinit lucid light-mode \
   atinit"zstyle ':autocomplete:*' min-input 1" \
   atinit"zstyle ':autocomplete:tab:*' widget-style menu-select" \
   atinit"zstyle ':autocomplete:tab:*' fzf-completion yes" \
-  atinit"zstyle ':autocomplete:tab:*' insert-unambiguous yes" \
   for marlonrichert/zsh-autocomplete
