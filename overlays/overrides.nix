2bca8fb0542807fbde34f0ffda220b73a749fb04
Thore Strassburg <thore@weilbier.net>
2021-06-28 11:28:57 +0200

    Reduce default overlay definitions
    Plenty of them are just not used anymore. So keep the repository clean and
    remove them.

--- overlays/overrides.nix
+++ overlays/overrides.nix
@@ -1,29 +1,8 @@
 channels: final: prev: {
-
-  __dontExport = true; # overrides clutter up actual creations
+  __dontExport = true;
 
   inherit (channels.latest)
     cachix
-    dhall
-    discord
-    element-desktop
-    manix
-    rage
+    rage # Used for the secret encryption in DevOS.
     nixpkgs-fmt
-    qutebrowser
-    signal-desktop
-    starship;
-
-
-  haskellPackages = prev.haskellPackages.override {
-    overrides = hfinal: hprev:
-      let version = prev.lib.replaceChars [ "." ] [ "" ] prev.ghc.version;
-      in
-      {
-        # same for haskell packages, matching ghc versions
-        inherit (channels.latest.haskell.packages."ghc${version}")
-          haskell-language-server;
-      };
-  };
-
 }
