a0fcc0bd2d1ff52a382586d469ce70e4f89bc6ab
Thore Strassburg <thore@weilbier.net>
2021-11-16 11:06:24 +0100

    zsh: prevent autocompletion of remote paths
    This is very annoying as I use an external hardware key for
    authentication. The pop-up menu to insert my key is interrupting the
    shell input all the time.

--- roles/zinit/files/plugin.d/90-autocomplete.zsh
+++ roles/zinit/files/plugin.d/90-autocomplete.zsh
@@ -1,6 +1,7 @@
 # Can't use `wait` mode, else hooks get not registered for the first prompt.
 zinit lucid light-mode \
   atinit"zstyle ':completion:*' extra-verbose yes" \
+  atinit"zstyle ':completion:*' remote-access no" \
   atinit"zstyle ':autocomplete:*' min-input 1" \
   atinit"zstyle ':autocomplete:tab:*' widget-style menu-select" \
   atinit"zstyle ':autocomplete:tab:*' fzf-completion yes" \
