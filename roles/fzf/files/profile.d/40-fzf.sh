8322b99465ad6430cbc1ea5821ffacd86aeaa503
Thore Strassburg <thore@strassburg.me>
2025-09-01 09:18:27 +0200

    feat(roles/fzf): improve configuration and integration with TMux

--- roles/fzf/files/profile.d/40-fzf.sh
+++ roles/fzf/files/profile.d/40-fzf.sh
@@ -7,12 +7,11 @@
 # systemd is environment-generator can't handle the whitespace characters well.
 # Decoding it in a not readable manner to make it working is not worth the
 # trouble.
-FZF_DEFAULT_OPTS='--height 40% --color fg:38,hl:191,fg+:81,bg+:16,info:197,prompt:197,spinner:197,pointer:197,marker:191,header:15'
+function shell_runs_in_tmux_session() { test -n "$TMUX"; }
 
-FZF_COMPLETION_TRIGGER='**'
-FZF_COMPLETION_OPTS='--preview "bat --color=always --line-range :50 {} 2> /dev/null || head -50 {}"'
-
-FZF_TMUX=1
-FZF_TMUX_OPTS='-p -w70% -h50% -xC -yC'
+FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --preview 'bat --color=always {} 2> /dev/null'"
+FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --bind 'ctrl-d:reload(fd . --type=directory),ctrl-f:reload(fd . --type=file)'"
+FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --color fg:38,hl:191,fg+:81,bg+:16,info:197,prompt:197,spinner:197,pointer:197,marker:191,header:15"
+shell_runs_in_tmux_session && FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --tmux center"
 
 source /usr/share/fzf/completion.zsh
