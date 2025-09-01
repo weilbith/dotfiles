b0673984bbf4a1da0677217d8f38fccb69bddc49
Thore Strassburg <thore@strassburg.me>
2025-09-01 09:51:08 +0200

    chore(roles/zinit): remove obsolete dotenv as superseed by direnv

--- roles/zinit/files/plugin.d/10-miscellaneous.zsh
+++ roles/zinit/files/plugin.d/10-miscellaneous.zsh
@@ -1,6 +1,5 @@
 zinit wait lucid light-mode for \
   OMZP::sudo \
-  OMZP::dotenv \
   OMZP::colored-man-pages \
   hlissner/zsh-autopair \
   blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions
